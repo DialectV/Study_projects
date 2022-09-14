c970c3dee0ecb8b924c1c92e30f15eda00011eff
Dr. Alexey <dronkin.alexei@mail.ru>
2022-09-14 21:05:13 +0400

    Just Hello World
    My first console app?

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Нажми <H>, чтобы поприветствовать мир");
+            if (Console.ReadKey().Key == ConsoleKey.H)
+            {
+                printHelloWorld();
+            }
+        }
+
+        static void printHelloWorld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+}
