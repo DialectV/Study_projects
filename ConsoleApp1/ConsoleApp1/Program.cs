04333b10ca3f99519d0667f333292cacb6b79390
Dr. Alexey <dronkin.alexei@mail.ru>
2022-09-15 22:48:05 +0400

    Created converter dec to hex
    I changed my first app and add new function for converting dec to hex

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,15 +7,46 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Нажми <H>, чтобы поприветствовать мир");
+            Console.WriteLine("Иначе будет запущен конвертор decimal в hex");
             if (Console.ReadKey().Key == ConsoleKey.H)
             {
                 printHelloWorld();
             }
+            else
+            {
+                Console.WriteLine("Введите целое десятичное число!");
+                Console.WriteLine($"Ваше число в 16-ой системе равно {decimalToHex(Console.ReadLine())}");
+            }
+
         }
 
         static void printHelloWorld()
         {
             Console.WriteLine("Hello World!");
+        }
+
+        static string decimalToHex(string input)
+        {
+            int x = int.Parse(input);
+
+            string answer = "";
+            string hexabc = "0123456789abcdef";
+
+            while (true)
+            {
+                if (x / 16 >= 1)
+                {
+                    answer = hexabc[x % 16] + answer;
+                    x /= 16;
+                }
+                else
+                {
+                    answer = hexabc[x % 16] + answer;
+                    break;
+                }
+            }
+
+            return answer;
         }
     }
 
